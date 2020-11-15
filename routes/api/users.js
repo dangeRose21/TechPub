7ba6b0117529a58dfd4c0d39312c1b8f4a4f9849
Diana Barrero <dangeRose21@users.noreply.github.com>
2020-11-15 16:02:10 -0500

    json token used to encrypt user data

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const gravatar = require('gravatar');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const { check, validationResult } = require('express-validator/check');
 
@@ -59,9 +61,21 @@ async (req, res) => {
         await user.save();
 
         //Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User registered');
-    
+        jwt.sign(
+            payload,
+             config.get('jwtSecret'),
+             { expiresIn: 360000 },
+             (err, token) => {
+                 if (err) throw err;
+                 res.json({ token });
+             } 
+             );
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
